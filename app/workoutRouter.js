ecd2ca255eef45a8aab015669881437f102e19a3
Jordi Kleriga <jordikleriga@gmail.com>
2018-12-25 23:30:32 -0600

    added delete and put router

--- app/workoutRouter.js
+++ app/workoutRouter.js
@@ -66,4 +66,62 @@ router.post('/' , jsonParser , (req, res) => {
         });
 });
 
+
+router.put('/:id', jsonParser, (req,res) => {
+
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    // we return here to break out of this function
+    return res.status(400).json({message: message});
+  }
+
+    Workout.findOne({_id:req.params.id})
+        .then(workout => {
+            if(workout != null && Object.keys(workout).length > 0) {
+                Workout.update({
+                    name: req.body.name,
+                    date: req.body.date,
+                    exercises: req.body.exercises
+                })
+                    .then(workout => {
+                        res.status(200).json({message: 'Workout updated!', workout})
+                    })
+                    .catch(err => {
+                        res.status(500).json({message: 'Could not update workout'})
+                    })
+            }
+            else {
+                res.status(500).json({message: 'Could not find workout to update'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Internal Server Error'})
+        })
+});
+
+
+router.delete('/:id', jsonParser, (req,res) => {
+
+    Workout.findOne({_id:req.params.id})
+        .then(workout => {
+            if(workout != null && Object.keys(workout).length > 0) {
+                Workout.deleteOne(workout)
+                    .then(res.status(400).json({message: 'Success'}))
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({message: 'Error deleting Workout'})
+                    })
+            }
+            else {
+                res.status(400).json({message: 'Workout does not exist'})
+            }})
+        .catch(err => {
+            console.log(err);
+            res.status(200).json({message: 'Could not find Workout'})
+        })
+});
+
 module.exports = { router };
